80ced273f6e025d0ad4cacf2ce725a9da3f80fa3
jh8057 <luckysymbol13@gmail.com>
2022-10-30 00:38:58 +0900

    nav bar

---
+++ src/components/navigationBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <span>HOME</span>
+    <span>TOTAL</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
+
+<style></style>
