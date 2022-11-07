590e218c8139ef8d24c70b1ffbf1b093d5055d61
jh8057 <luckysymbol13@gmail.com>
2022-11-07 23:41:05 +0900

    [TextImage] init

---
+++ src/views/TextImage.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Text Image</h1>
+  <main>
+    <section>image1</section>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
+
+<style></style>
