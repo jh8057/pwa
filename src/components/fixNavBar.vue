6e8dc3e544132d5ec56a2de83d8ba37d522ce5e2
jh8057 <luckysymbol13@gmail.com>
2022-11-14 21:57:52 +0900

    [nav] bar

--- src/components/fixNavBar.vue
+++ src/components/fixNavBar.vue
@@ -1,14 +1,15 @@
 <template>
   <ul>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
+    <li>Home</li>
+    <li>a</li>
+    <li>b</li>
+    <li>More</li>
   </ul>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
 </script>
 
 <style></style>
