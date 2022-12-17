87b37e53840280df2212d5d2c9d3709b7d15443e
jh8057 <luckysymbol13@gmail.com>
2022-12-18 00:42:23 +0900

    [chart] change variable

--- src/components/composable/useChartjs.ts
+++ src/components/composable/useChartjs.ts
@@ -1,44 +1,51 @@
 import { reactive, toRefs } from "vue";
 export default () => {
+  const labels = [
+    "NUMBER#1-ORANGE",
+    "NUMBER#2-CORAL",
+    "NUMBER#3-SALMON",
+    "NUMBER#4-WHEAT",
+  ];
+  const backgroundColor = ["#FFA500", "#FF7F50", "#E18B6B", "#F5DEB3"];
+
   const state = reactive({
     chartData: {
-      labels: ["NUMBER#1-ORANGE", "NUMBER#2-CORAL", "NUMBER#3-SALMON", "NUMBER#4-WHEAT"],
+      labels,
       datasets: [
         {
-          backgroundColor: ["#FFA500", "#FF7F50", "#E18B6B", "#F5DEB3"],
+          backgroundColor,
           data: [40, 20, 80, 10],
         },
       ],
     },
-    charOption : {
+    charOption: {
       responsive: true,
       maintainAspectRatio: false,
-    }
-  
+    },
   });
 
-  const changeChartDataSize = () =>{
+  const changeChartDataSize = () => {
     //create random Data
-    let newData = []
-    for(let i = 0; i < 4;i++){
-      let randomNum = Math.floor(Math.random() * 100)
-      newData.push(randomNum)
+    let newData = [];
+    for (let i = 0; i < 4; i++) {
+      let randomNum = Math.floor(Math.random() * 100);
+      newData.push(randomNum);
     }
 
     //change chart Data
     state.chartData = {
-      labels: ["NUMBER#1-ORANGE", "NUMBER#2-CORAL", "NUMBER#3-SALMON", "NUMBER#4-WHEAT"],
+      labels,
       datasets: [
         {
-          backgroundColor: ["#FFA500", "#FF7F50", "#E18B6B", "#F5DEB3"],
-          data: newData
+          backgroundColor,
+          data: newData,
         },
       ],
-    }
-  }
+    };
+  };
 
   return {
     ...toRefs(state),
-    changeChartDataSize
+    changeChartDataSize,
   };
 };
