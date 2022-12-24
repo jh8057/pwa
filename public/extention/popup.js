bcfaf404c455f2c210cd7382c86e4674d6f321e4
jh8057 <luckysymbol13@gmail.com>
2022-12-24 23:19:50 +0900

    [chrome] extension
    - make function about find url & run when tab is updated

--- public/extention/popup.js
+++ public/extention/popup.js
@@ -1,1 +1,22 @@
-console.log("THIS SIS POPUP");
+// find 'youtube' url on tab list
+chrome.tabs.query({}, function (tabs) {
+  console.log("======");
+  tabs.forEach(function (tab) {
+    console.log(tab.url);
+    if (tab.url && tab.url.indexOf("youtube") !== -1) {
+      let YOUTUBE = new Date();
+
+      const ele = document.getElementById("url__youtube");
+      ele.innerHTML = YOUTUBE;
+    }
+  });
+});
+
+// run when tab is updated
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status == "complete" && tab.active) {
+    // do your things
+    alert("heell");
+    console.log("TTT");
+  }
+});
